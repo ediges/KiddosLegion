79f955c77c6a7a6b0dd6615353bb2dbd13a30e34
Eduard Gesyuk <eduardge@gmail.com>
2023-07-22 08:01:18 -0400

    Update SharedDriver.java
    options.addArguments("--headless"); commented out

--- src/main/java/Utils/SharedDriver.java
+++ src/main/java/Utils/SharedDriver.java
@@ -23,7 +23,7 @@ public class SharedDriver {
                 ChromeOptions options = new ChromeOptions();
                 options.addArguments("--no-sandbox");
                 options.addArguments("--disable-dev-shm-usage");
-                options.addArguments("--headless");
+                // options.addArguments("--headless");
 
                 WebDriverManager.chromedriver().setup();
                 webDriver = new ChromeDriver();
