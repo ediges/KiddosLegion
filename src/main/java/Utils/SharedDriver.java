2dd2ec325fa100f40421599c11308231a11dc744
Eduard Gesyuk <eduardge@gmail.com>
2023-07-22 08:22:16 -0400

    Update SharedDriver.java
    import org.openqa.selenium.edge.EdgeDriver;

--- src/main/java/Utils/SharedDriver.java
+++ src/main/java/Utils/SharedDriver.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import java.util.concurrent.TimeUnit;
 
